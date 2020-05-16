88a8ab3a03410b4d7337ccb6c180c1fad0b61915
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-16 18:03:36 +0300

    Add javascript for liking and unliking posts

--- application/static/main.js
+++ application/static/main.js
@@ -1,6 +1,20 @@
 const handleLike = (postId) => {
-  const likeLabel = document.querySelector(`#likes-label-${postId}`)
-  fetch(`posts/${postId}`, { method: 'POST' }).then((response) => {
-    likeLabel.innerHTML = Number(likeLabel.innerHTML) + 1
-  })
+  const likesText = document.querySelector(`#likes-text-${postId}`)
+  const likesLabel = document.querySelector(`#likes-label-${postId}`)
+
+  if (!likesLabel.classList.contains('red')) {
+    fetch(`/posts/${postId}/likes`, { method: 'PUT' }).then((response) => {
+      if (response.ok) {
+        likesText.innerHTML = Number(likesText.innerHTML) + 1
+        likesLabel.classList.add('red')
+      }
+    })
+  } else {
+    fetch(`/posts/${postId}/likes`, { method: 'DELETE' }).then((response) => {
+      if (response.ok) {
+        likesText.innerHTML = Number(likesText.innerHTML) - 1
+        likesLabel.classList.remove('red')
+      }
+    })
+  }
 }
