3ade069702dccc539d5a16de32e361663d491d03
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-16 18:00:21 +0300

    Rename database file

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
