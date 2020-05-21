edd1898ce23c7fb2da74018f1605718993f318f0
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-21 11:35:15 +0300

    Register login manager

--- application/__init__.py
+++ application/__init__.py
@@ -12,7 +12,26 @@ from application import views
 
 from application.posts import models
 from application.posts import views
+
 from application.topics import models
 from application.topics import views
+
+from application.auth import models
+from application.auth import views
+
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "You must be logged in to use this functionality"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id) 
 
 db.create_all()
