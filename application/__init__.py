6eb15fe06bc7066de26ef2c19749a5c708d8b8ff
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-27 14:48:02 +0300

    Register Heroku database

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -34,4 +39,7 @@ login_manager.login_message = "You must be logged in to use this functionality"
 def load_user(user_id):
     return User.query.get(user_id) 
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
