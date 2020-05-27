e6854a38e57f22daa4f12708dd12994b2331cc87
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-27 14:52:37 +0300

    Add tags field

--- application/topics/forms.py
+++ application/topics/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, validators, TextAreaField
+from wtforms import PasswordField, StringField, validators, TextAreaField, SelectMultipleField
 
 
 class TopicForm(FlaskForm):
@@ -7,6 +7,7 @@ class TopicForm(FlaskForm):
         "Subject", [validators.Length(max=100), validators.DataRequired()], render_kw={"placeholder": "Subject"})
     body = TextAreaField("Body", [validators.Length(
         max=1000), validators.DataRequired()], render_kw={"placeholder": "Type your message here"})
+    tags = SelectMultipleField('Tags', choices=[], validate_choice=False)
 
     class Meta:
         csrf = False
