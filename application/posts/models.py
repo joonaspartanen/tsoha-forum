d26341c8320a6562f0053c132ac1a321d50669f6
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-27 14:51:02 +0300

    Add post_likes and author properties

--- application/posts/models.py
+++ application/posts/models.py
@@ -1,8 +1,18 @@
 from application import db
+from datetime import datetime
+
+from sqlalchemy.sql import text
+
+post_likes = db.Table("post_likes",
+                      db.Column("post_id", db.Integer,
+                                db.ForeignKey("posts.id")),
+                      db.Column("user_id", db.Integer,
+                                db.ForeignKey("accounts.id"))
+                      )
 
 
 class Post(db.Model):
-    __tablename__ = 'posts'
+    __tablename__ = "posts"
 
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
@@ -12,11 +22,15 @@ class Post(db.Model):
     body = db.Column(db.String(1000), nullable=False)
     likes = db.Column(db.Integer, default=0)
 
-    topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'), nullable=False)
-    topic = db.relationship('Topic', back_populates='posts')
+    likedByUsers = db.relationship("User", secondary=post_likes)
 
-    author_id = db.Column(db.Integer, db.ForeignKey('accounts.id'), nullable=False)
-    author = db.relationship('User', back_populates='posts')
+    topic_id = db.Column(db.Integer, db.ForeignKey(
+        "topics.id"), nullable=False)
+    topic = db.relationship("Topic", back_populates="posts")
+
+    author_id = db.Column(db.Integer, db.ForeignKey(
+        "accounts.id"), nullable=False)
+    author = db.relationship("User", back_populates="posts")
 
     def __init__(self, body):
         self.body = body
