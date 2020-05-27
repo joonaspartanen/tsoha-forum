935a6a88516f75b71b51b9b09dbfa0312391c156
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-27 14:52:59 +0300

    Add relationship to tags

--- application/topics/models.py
+++ application/topics/models.py
@@ -1,8 +1,10 @@
 from application import db
 
+from application.tags.models import tag_topics
+
 
 class Topic(db.Model):
-    __tablename__ = 'topics'
+    __tablename__ = "topics"
 
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
@@ -11,8 +13,9 @@ class Topic(db.Model):
 
     subject = db.Column(db.String(100), nullable=False)
 
-    posts = db.relationship('Post', back_populates='topic')
+    posts = db.relationship("Post", back_populates="topic")
+
+    tags = db.relationship("Tag", secondary=tag_topics)
 
     def __init__(self, subject):
         self.subject = subject
-
