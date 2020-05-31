2a88e38b94aa80483eba2258396e449add121b6f
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-31 20:41:22 +0300

    Fix SQL query to include users with no posts

--- application/auth/models.py
+++ application/auth/models.py
@@ -40,13 +40,15 @@ class User(db.Model):
     @staticmethod
     def get_user_with_statistics(user_id):
         stmt = text("SELECT Accounts.id, Accounts.username, Accounts.description, Accounts.is_admin, Accounts.date_created, "
-                    "Post_likes.likes, Posts.posts_amount FROM Accounts JOIN (SELECT author_id, COUNT(post_id) AS likes "
-                    "FROM Post_likes JOIN Posts ON Post_likes.post_id = Posts.id GROUP BY author_id) AS Post_likes "
-                    "ON Post_likes.author_id = Accounts.id JOIN (SELECT author_id, COUNT(id) AS posts_amount FROM Posts "
-                    "GROUP BY author_id) AS Posts ON Accounts.id = Posts.author_id WHERE Posts.author_id = :user_id;").params(user_id=user_id)
+                    "Post_likes.likes, Posts.posts_amount FROM Accounts LEFT JOIN (SELECT author_id, COUNT(post_id) AS likes "
+                    "FROM Post_likes LEFT JOIN Posts ON Post_likes.post_id = Posts.id GROUP BY author_id) AS Post_likes "
+                    "ON Post_likes.author_id = Accounts.id LEFT JOIN (SELECT author_id, COUNT(id) AS posts_amount FROM Posts "
+                    "GROUP BY author_id) AS Posts ON Accounts.id = Posts.author_id WHERE Accounts.id = :user_id;").params(user_id=user_id)
 
         result = db.engine.execute(stmt)
         row = result.first()
+        mapped_row = [0 if item is None else item for item in row]
 
-        return {"id": row[0], "username": row[1], "description": row[2],
-                "is_admin": row[3], "date_created": Time_formatter.get_timestamp(row[4]), "likes": row[5], "posts_amount": row[6]}
+        return {"id": mapped_row[0], "username": mapped_row[1], "description": mapped_row[2],
+                "is_admin": mapped_row[3], "date_created": Time_formatter.get_timestamp(mapped_row[4]),
+                "likes": mapped_row[5], "posts_amount": mapped_row[6]}
