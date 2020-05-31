8a7b698dc5e79a94580289d59d5fb8ac18651797
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-31 18:53:43 +0300

    Create form for editing user info

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,11 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, validators
+from wtforms import PasswordField, StringField, TextAreaField, validators
 
 
 class UserForm(FlaskForm):
     username = StringField("Username", [validators.Length(
-        min=5)], render_kw={"placeholder": "Username"})
-    password = PasswordField("Password", [validators.Length(min=8)], render_kw={"placeholder": "Password"})
+        min=5), validators.DataRequired()], render_kw={"placeholder": "Username"})
+    password = PasswordField("Password", [validators.Length(min=8), validators.DataRequired()], render_kw={
+                             "placeholder": "Password"})
+
+    class Meta:
+        csrf = False
+
+
+class EditProfileForm(FlaskForm):
+    description = TextAreaField("Description", [validators.Length(
+        max=1000), validators.DataRequired()], render_kw={"placeholder": "Describe yourself"})
 
     class Meta:
         csrf = False
