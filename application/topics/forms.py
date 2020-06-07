17cedc3143aa0cbb68e64fcfa781d1861701577c
Joonas Partanen <partanen.joonas@gmail.com>
2020-06-07 18:58:36 +0300

    Create search form

--- application/topics/forms.py
+++ application/topics/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, validators, TextAreaField, SelectMultipleField
+from wtforms import PasswordField, StringField, validators, TextAreaField, SelectField, SelectMultipleField
 from wtforms.validators import ValidationError
 
 
 class TopicForm(FlaskForm):
     subject = StringField(
         "Subject", [validators.Length(max=100), validators.DataRequired()], render_kw={"placeholder": "Subject"})
-    body = TextAreaField("Body", [validators.Length(
+    body = TextAreaField("Message body", [validators.Length(
         max=1000), validators.DataRequired()], render_kw={"placeholder": "Type your message here"})
     tags = SelectMultipleField('Tags', choices=[], validate_choice=False)
 
@@ -16,8 +16,18 @@ class TopicForm(FlaskForm):
     def validate_tags(self, field):
         tag_names = field.data[0].split(",")
         if (len(field.data[0]) == 0):
-            raise ValidationError('Please provide at least one tag to describe the topic.')
+            raise ValidationError(
+                'Please provide at least one tag to describe the topic.')
 
         for tag_name in tag_names:
             if len(tag_name) > 10:
                 raise ValidationError('A tag can be only 10 characters long.')
+
+
+class SearchForm(FlaskForm):
+    subject = StringField("Subject contains", [validators.Length(
+        max=100)], render_kw={"placeholder": "Subject"})
+    author = StringField("Author name contains", [validators.Length(max=100)], render_kw={"placeholder": "Author"})
+
+    class Meta:
+        csrf = False
